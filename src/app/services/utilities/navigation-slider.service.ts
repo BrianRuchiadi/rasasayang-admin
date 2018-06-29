25413f7358e81a85c05fd74066d302532299ea10
Brian Ruchiadi <ruchiadibrian@yahoo.com>
2018-06-29 15:58:16 +0700

    debounce click with control variables

--- src/app/services/utilities/navigation-slider.service.ts
+++ src/app/services/utilities/navigation-slider.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
 
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, tap, debounceTime } from 'rxjs/operators';
 import { Notification } from './../../classes/notification';
 import { Subject } from 'rxjs';
 
 @Injectable()
 export class NavigationSliderService {
     public observableSlideSubject = new Subject<Boolean>();
+    public clicked = false;
+    public canClick = true;
     public slideState = true;
     public slideStateObservable = this.observableSlideSubject.asObservable();
 
     public changeSlideState(): void {
-        this.slideState = !this.slideState;
+        if (!this.clicked) {
+            this.clicked = true;
 
-        this.observableSlideSubject.next(this.slideState);
+            this.canClick = false;
+            this.slideState = !this.slideState;
+            this.observableSlideSubject.next(this.slideState);
+
+            this.waitUntilCanClick();
+            return;
+        }
+
+        if (this.canClick) {
+            this.canClick =  false;
+            this.slideState = !this.slideState;
+            this.observableSlideSubject.next(this.slideState);
+
+            this.waitUntilCanClick();
+        }
+    }
+
+    public waitUntilCanClick() {
+        setTimeout(() => this.canClick = true, 1000);
     }
 }
